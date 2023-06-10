5fa578374b6e9d1e529cd31385bc7b591d9e756a
abdulmex <steveabdulmex@gmail.com>
2023-06-10 21:44:01 +0100

    E-commerce-Sneakers-brand

--- Js/cart.js
+++ Js/cart.js
@@ -2,3 +2,6 @@
 const cart = document.querySelector(".cart");
 // Get the product element that was clicked.
 const product = event.target.closest(".product");
+// Get the product name and price.
+const name = product.querySelector(".name").textContent;
+const price = product.querySelector(".price").textContent;
