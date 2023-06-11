e36146a24624c5c4142a4cadd1c4e7b0247a1725
abdulmex <steveabdulmex@gmail.com>
2023-06-11 23:20:42 +0100

    E-commerce-Sneakers-brand

--- Js/cart.js
+++ Js/cart.js
@@ -10,14 +10,12 @@ function addItemToCart(item) {
 document.querySelector(".add-to-cart").addEventListener("click", () => {
   // Get the product ID from the button.
   let productId = this.dataset.productId;
-
   // Get the product details from the server.
   fetch(`/api/products/${productId}`)
     .then(response => response.json())
     .then(product => {
       // Add the product to the cart.
       addItemToCart(product);
-
       // Show a success message.
       alert("Product added to cart!");
     });
