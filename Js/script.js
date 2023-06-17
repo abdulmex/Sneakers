1af175723eaa1f0fd1af9ac01e7df91ffc550637
abdulmex <steveabdulmex@gmail.com>
2023-06-17 17:57:10 +0100

    E-commerce-Sneakers-brand

--- Js/script.js
+++ Js/script.js
@@ -1,15 +1,4 @@
-// Make a GET request to the Sneakers API
 fetch('https://api.thesneakerdatabase.com/v1/sneakers')
-  .then(response => response.json())
-  .then(data => {
-    // Process the response data
-    const sneakers = data.results;
-
-    // Use the sneakers data as needed
-    console.log(sneakers);
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
-
-  
+.then(response => {
+    console.log(response)
+})
