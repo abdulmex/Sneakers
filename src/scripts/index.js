7550483e6c416cc771eed43c8c062a8b13e038fe
abdulmex <steveabdulmex@gmail.com>
2023-08-22 00:28:21 +0100

    Js

---
+++ src/scripts/index.js
@@ -0,0 +1,18 @@
+const modeToggle = document.getElementById("mode-toggle");
+const prefersDarkMode = window.matchMedia(
+  "(prefers-color-scheme: dark)"
+).matches;
+let darkMode =
+  localStorage.getItem("darkMode") || (prefersDarkMode ? "dark" : "light");
+
+const applyMode = () => {
+  document.body.classList.toggle("dark-mode", darkMode === "dark");
+};
+
+applyMode();
+
+modeToggle.addEventListener("click", () => {
+  darkMode = darkMode === "dark" ? "light" : "dark";
+  applyMode();
+  localStorage.setItem("darkMode", darkMode);
+});
